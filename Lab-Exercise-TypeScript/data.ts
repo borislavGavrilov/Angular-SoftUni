c57f6f0703d2103a3e8cc46fb4bb829cf80920c8
Borislav Gavrilov <136329010+borislavGavrilov@users.noreply.github.com>
2025-07-01 21:03:38 +0300

    Update data.ts

--- Lab-Exercise-TypeScript/data.ts
+++ Lab-Exercise-TypeScript/data.ts
@@ -1,21 +1,20 @@
-class Data {
-
+class RequestData {
     method: string;
-    url: string;   
+    url: string;
     version: string;
     message: string;
-    response: string | undefined
-    fullfilled: boolean;
+    //response?: string;
+    fulfilled: boolean;
 
-    constructor(method: string, url: string, version: string, message: string, response?: string) {
+    constructor(method: string, url: string, version: string, message: string,){ //response?: string) {
         this.method = method;
         this.url = url;
         this.version = version;
         this.message = message;
-        this.response = undefined;
-        this.fullfilled = false;
+        //this.response = response;
+        this.fulfilled = false;
     }
 }
 
-const result = new Data('GET', 'https://api.example.com/data', 'HTTP/1.1', 'Request successful');
-console.log(result);
+const resultt = new RequestData('GET', 'https://api.example.com/data', 'HTTP/1.1', 'Request successful');
+console.log(resultt)
