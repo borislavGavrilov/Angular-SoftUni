d8c5082dd1add5a06ccfb996fc8c168cb2a3164a
Borislav Gavrilov <136329010+borislavGavrilov@users.noreply.github.com>
2025-07-01 23:25:19 +0300

    some test in angular demo

--- my-first-angular/src/app/app.component.ts
+++ my-first-angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GreetingComponent } from './greeting/greeting.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , GreetingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ my-first-angular/src/app/greeting/greeting.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting',
+  imports: [],
+  templateUrl: './greeting.component.html',
+  styleUrl: './greeting.component.css'
+})
+export class GreetingComponent {
+ 
+  name: string = 'World';
+
+}
